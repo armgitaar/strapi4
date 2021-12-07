836b61177f9b13babdecc7e90f3897f4ab41965c
Adam Miedema <armgitaar@yahoo.com>
2021-12-07 15:08:30 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,13 +4,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: process.env('DATABASE_HOST', '127.0.0.1'),
-      port: process.env.int('DATABASE_PORT', 5432),
-      database: process.env('DATABASE_NAME', 'strapi'),
-      user: process.env('DATABASE_USERNAME', 'strapi'),
-      password: process.env('DATABASE_PASSWORD', 'strapi'),
+      host: process.env.DATABASE_HOST || '127.0.0.1',
+      port: process.env.DATABASE_PORT || 5432,
+      database: process.env.DATABASE_NAME || 'strapi',
+      user: process.env.DATABASE_USERNAME || 'strapi',
+      password: process.env.DATABASE_PASSWORD || 'strapi',
       ssl: {
-        rejectUnauthorized: process.env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        rejectUnauthorized: process.env.DATABASE_SSL_SELF || false, // For self-signed certificates
       },
     },
     debug: false,
