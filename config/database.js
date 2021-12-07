1473cccb9b20fa46c066892ac20c3e37a8349040
Adam Miedema <armgitaar@yahoo.com>
2021-12-07 15:50:13 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
     client: 'mysql',
     connection: {
       host: process.env.DATABASE_HOST || '127.0.0.1',
-      port: process.env.DATABASE_PORT || 5432,
+      port: process.env.DATABASE_PORT || 3306,
       database: process.env.DATABASE_NAME || 'strapi',
       user: process.env.DATABASE_USERNAME || 'strapi',
       password: process.env.DATABASE_PASSWORD || 'strapi',
